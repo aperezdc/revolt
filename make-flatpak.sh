b20b01de87427832d20e649db23ee3e7382257b8
Adrian Perez de Castro <aperez@igalia.com>
2016-10-13 02:32:48 +0300

    make-flatpak.sh: Use flatpak-builder instead of manually building

--- make-flatpak.sh
+++ make-flatpak.sh
@@ -1,39 +1,26 @@
 #! /bin/bash
 set -e
 
-readonly NAME='com.igalia.VectorGnome'
 readonly BUILDDIR="$(dirname "$0")/.flatpak-build"
 readonly REPODIR="$(dirname "$0")/.flatpak-repo"
-readonly GNOMEVER='3.22'
 
 cleanup () {
 	rm -rf "${BUILDDIR}"
 }
 trap cleanup EXIT
 
-
-flatpak build-init "${BUILDDIR}" "${NAME}" \
-	org.gnome.Sdk org.gnome.Platform "${GNOMEVER}"
-
-flatpak build "${BUILDDIR}" ./install.sh --install --prefix=/app --destdir=/
-flatpak build-finish "${BUILDDIR}" \
-	--device=dri \
-	--share=network \
-	--share=ipc \
-	--socket=x11 \
-	--socket=pulseaudio \
-	--socket=session-bus \
-	--own-name="${NAME}" \
-	--talk-name=org.freedesktop.Notifications \
-	--talk-name=org.gtk.Notifications \
-	--command=vector
-flatpak build-export "${REPODIR}" "${BUILDDIR}"
+declare -a buildargs=(
+	--sandbox
+	--force-clean
+	--require-changes
+	--repo="${REPODIR}"
+	--subject="Vector-GNOME $(date +%Y%m%d).$(git describe --always --tags)"
+)
 
 if [[ -n ${EMAIL} ]] ; then
-	flatpak build-sign --gpg-sign="${EMAIL}" "${REPODIR}" "${NAME}"
-	flatpak build-update-repo --title=Vector --gpg-sign="${EMAIL}" "${REPODIR}"
-else
-	flatpak build-update-repo --title=Vector "${REPODIR}"
+	buildargs+=( --gpg-sign="${EMAIL}" )
 fi
 
-flatpak build-bundle "${REPODIR}" "${NAME}.flatpak" "${NAME}"
+set -x
+exec flatpak-builder "${buildargs[@]}" "${BUILDDIR}" com.igalia.VectorGnome.json
+
