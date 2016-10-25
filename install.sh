a945a9791e021248f2fbd006d1df29acb9bd7440
Adrian Perez de Castro <aperez@igalia.com>
2016-10-26 01:36:22 +0300

    Fix sizes of installed icons

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ install-setup "$0" "$@"
 install-bin riot
 install-desktop-file com.igalia.VectorGnome.desktop
 
-for size in 32 48 64 128 ; do
+for size in 16 32 64 128 256 ; do
 	install-icon com.igalia.VectorGnome ${size} apps assets/riot-${size}.png
 done
 install-icon com.igalia.VectorGnome scalable apps assets/riot.svg
