86bf40c50137c6d6b00e3739ae7be73930d3664e
Adrian Perez de Castro <aperez@igalia.com>
2017-03-27 19:43:36 +0300

    syncrepo.sh: Add script to synchronize Flatpak repository

---
+++ syncrepo.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+#
+# sync-repo.sh
+# Copyright (C) 2016 Adrian Perez <aperez@igalia.com>
+#
+# Distributed under terms of the MIT license.
+#
+set -e
+
+LOCAL="$(dirname "$0")/.flatpak-repo/"
+REMOTE='aperez@perezdecastro.org:/srv/http/flatpak.perezdecastro.org/revolt/'
+
+RSYNC_REPOS=$(type -P ostree-rsync-repos rsync-repos | head -1)
+if [[ -z ${RSYNC_REPOS} ]] ; then
+	echo "$0: The ostree-rsync-repos (or rsync-repos) tool is not installed."
+	echo 'Please install from https://github.com/ostreedev/ostree-releng-scripts'
+	echo 'and make sure the tools are in your $PATH'
+	exit 1
+fi 1>&2
+
+declare -a rsync_command=( "${RSYNC_REPOS}" )
+
+case $1 in
+	push)
+		rsync_command+=( --src "${LOCAL}" --dest "${REMOTE}" )
+		;;
+	pull)
+		rsync_command+=( --src "${REMOTE}" --dest "${LOCAL}" )
+		;;
+	*)
+		echo "Usage: $0 push|pull" 1>&2
+		exit 1
+		;;
+esac
+exec "${rsync_command[@]}"
