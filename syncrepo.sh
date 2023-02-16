9e4981696ff070cb11ba2fdabbb01a6f34eeebc1
Adrian Perez de Castro <aperez@igalia.com>
2023-02-16 16:00:50 +0200

    syncrepo.sh: Use rsync-repos from a local checkout
    Allow using the rsync-repos script from a local checkout of the releng
    scripts repository. While at it, add the script as a submodule for
    convenience.

--- syncrepo.sh
+++ syncrepo.sh
@@ -10,7 +10,8 @@ set -e
 LOCAL="$(dirname "$0")/.flatpak-repo/"
 REMOTE='aperez@perezdecastro.org:/srv/http/flatpak.perezdecastro.org/revolt/'
 
-RSYNC_REPOS=$(type -P ostree-rsync-repos rsync-repos | head -1)
+RSYNC_REPOS=$(type -P ostree-rsync-repos rsync-repos \
+	"$(dirname "$0")/ostree-releng-scripts/rsync-repos" | head -1)
 if [[ -z ${RSYNC_REPOS} ]] ; then
 	echo "$0: The ostree-rsync-repos (or rsync-repos) tool is not installed."
 	echo 'Please install from https://github.com/ostreedev/ostree-releng-scripts'
